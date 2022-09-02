5a0b38d45294fceb75458cbcdd3049eed94faa71
Antony Bailey <antony_bailey@waters.com>
2022-09-02 10:07:39 +0100

    Typos

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "cluster_name" {
   value       = data.aws_eks_cluster.cluster.name
-  description = "The EKS Cluster we deployed Linkerd to."
+  description = "The EKS Cluster we deployed Litmus to."
 }
